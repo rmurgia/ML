P_F computed correctly, normalization to be fixed

compute_PF.py
@@ -7,6 +7,7 @@
 import numpy as np
 import os, sys
 import matplotlib.pyplot as plt
+from lmfit import Minimizer, Parameters, report_fit
 
 flux_path = '../ML_catalogues/'
 
@@ -18,18 +19,31 @@ root = 'NCDM_'
 sims = [1] #how many sims
 nbins = 2048
 nlos = 5000
+PF_matrix = np.zeros((nlos,nbins))
+PF = np.zeros(nbins)
 F_obs_list = [0.669181, 0.617042, 0.564612, 0.512514, 0.461362, 0.411733, 0.364155, 0.253828, 0.146033, 0.0712724]
 z_obs_list = [3.0, 3.2, 3.4, 3.6, 3.8, 4.0, 4.2, 4.6, 5.0, 5.4]
+BoxSize = 20.
+kF = 2.0*np.pi/BoxSize #fundamental frequency  
+middle = nbins/2  
+kN = middle*kF #Nyquist frequency
+time_step = 0.5/kN
 
 TEST = 'EJA' # = 'EJA' to make test on 2 z-bins only
 
-############################################################################################
+###################################################################
+def func_A(A, tau_sim_list, meanF_obs, meanF_sim):
+	factor = meanF_sim/meanF_obs
+	numerator = np.sum(np.exp(-tau_sim_list))
+	denominator = np.sum(np.exp(-tau_sim_list/A))
+	result = np.abs(factor - (numerator/denominator))
+	return result
 
 ############################### loop on sims and redshifts
 
 if TEST == 'EJA':
 
-	zbins = [2.2]
+	zbins = [3.0]
 	print("You have only "+str(np.shape(zbins))+" redshift bin because it's a test!")
 	print('num of redshift bins = '+np.str(np.shape(zbins)))
 	print('num of sims = '+str(len(sims)))
@@ -46,7 +60,7 @@ else:
 for sim_index in sims:   #loop on sims
 
 	sim_folder = out_path+root+str(sim_index)
-	out_folder = sim_folder+'/PF'	#input/output folder
+	out_folder = sim_folder+'/PF/'	#input/output folder
 	if not os.path.exists(out_folder):
 		os.makedirs(out_folder)
 
@@ -63,26 +77,75 @@ for sim_index in sims:   #loop on sims
 		print("<F> ="+str(mean_flux))
 		print("observed <F> ="+str(mean_flux_obs))
 
-		A = np.log(mean_flux/mean_flux_obs) #compute norm. factor
+		if mean_flux > mean_flux_obs:
+			A_list = np.linspace(0.7,1., num=2000)
+			print("A < 1")
+		
+		elif mean_flux < mean_flux_obs:
+			A_list = np.linspace(1.,1.3, num=2000)
+			print("A > 1")
+		
+		y = []
+		for i,A in zip(range(len(A_list)),A_list):
+			y.append(func_A(A, tau_array, mean_flux_obs, mean_flux))
+		
+		index = np.where(y == min(y))
+		best_A = A_list[index][0]
+		# np.savetxt("prova2", np.transpose([A_list,y]))
 
-		print("norm. factor A ="+str(A))
+		print("norm. factor A ="+str(best_A))
 
 		flux_array_new = np.zeros(len(flux_array))
 		delta_array = np.zeros(len(flux_array))
 
-		flux_array_new[:] = np.exp(-A*tau_array[:])  #new flux array (normalized to the obs. flux)
-		delta_array[:] = (flux_array_new[:]- mean_flux_obs)/mean_flux_obs #1D density field
+		flux_array_new[:] = np.exp(-tau_array[:]/best_A)  #new flux array (normalized to the obs. flux)
+		mean_flux_new = np.mean(flux_array_new)
+		print("<F>_NEW ="+str(mean_flux_new))
+		delta_array[:] = (flux_array_new[:] - mean_flux_new)/mean_flux_new #1D density field
+		
+		print("len(deltas) = "+str(len(delta_array)))
+		
+		#compute FFT and P_flux per each los
+		for i in range(nlos):		
+			deltas = delta_array[nbins*i:nbins*(i+1)]
+			
+			if len(deltas) != nbins:
+				print("ERROR!!"); break
 
-		print("doing FFT...")
-		##compute 1D fft of delta_array
-		FFT = np.fft.fft(delta_array)
-		print("..done!")
+			##compute 1D fft of delta_array
+			FFT = np.fft.fft(deltas)
+			
+			##compute 1D power spectrum
+			PF_matrix[i][:] = np.abs(FFT)**2
+			
+		print(len(PF))
 
-		print("compute P_F...")
-		##compute 1D power spectrum
-		PF = np.abs(FFT)**2
-		print("..done!")
+		PF = PF_matrix[0][:]
+		freqs = np.fft.fftfreq(deltas.size, time_step)
+		idx = np.argsort(freqs)
 
-		plt.loglog(PF)
+		print(np.shape(freqs))
+		print(freqs)
+		print(np.shape(PF))
+		print(PF)
+		
+		## average over the nlos different P_flux
+		to_be_avg = np.zeros(nlos)
+		for i in range(nbins):
+		 	to_be_avg = PF_matrix[:,i]
+		 	PF[i] = np.mean(to_be_avg)
+		
+		print(len(to_be_avg))
+
+		end = int(0.5*nbins+1)
+		##converting from dimension-less power spectrum to P_F(k) in (h/Mpc) IN 1-DIM
+		freqs_final = np.abs(freqs[1:end])
+		PF_final = np.zeros(len(freqs_final))
+		PF_final[:] = 2*np.pi*PF[1:end]/freqs_final[:]
+
+		np.savetxt(out_folder+"PF_z3.dat",np.transpose([freqs_final,PF_final]))
+
+		plt.loglog(np.abs(freqs_final),PF_final)
+		x,y = np.loadtxt("/Users/rmurgia/Dropbox/SISSA/project/INTERPOLATION/NCDM_TEST/NCDM_TEST_1pf_z=3.0.txt", usecols=[0,1], unpack=True)
+		plt.loglog(x,y)
 		plt.show()
-